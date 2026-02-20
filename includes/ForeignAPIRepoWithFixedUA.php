ac6b936f27dc30ea4a2bee529c11e29a9dac08a2
Eric Hocking <ehock7229@outlook.com>
2026-02-20 11:10:42 -0500

    Adds GlobalUserPage hook and IRC formatter
    Adds a hook to enable filtering of wikis displayed on the GlobalUserPage based on extension availability.
    
    Also introduces a custom IRC formatter that prefixes messages with the wiki ID and excludes renameuser events from Trust and Safety.
    
    Implements a fixed User-Agent string for ForeignAPIRepo requests.

---
+++ includes/ForeignAPIRepoWithFixedUA.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wikivy\WikivyMagic;
+
+use MediaWiki\FileRepo\ForeignAPIRepo;
+use MediaWiki\WikiMap\WikiMap;
+use const MW_VERSION;
+
+class ForeignAPIRepoWithFixedUA extends ForeignAPIRepo
+{
+	public static function getUserAgent(): string {
+		$mediaWikiVersion = MW_VERSION;
+		$wikiId = WikiMap::getCurrentWikiId();
+		return "QuickInstantCommons/$mediaWikiVersion MediaWiki/$mediaWikiVersion $wikiId (https://wikivy.com; tech@wikivy.com)";
+	}
+
+}
---
+++ includes/WikivyIRCRCFeedFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wikivy\WikivyMagic;
+
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
+use MediaWiki\RCFeed\IRCColourfulRCFeedFormatter;
+use MediaWiki\RecentChanges\RecentChange;
+use MediaWiki\WikiMap\WikiMap;
+
+class WikivyIRCRCFeedFormatter extends IRCColourfulRCFeedFormatter
+{
+	/** @inheritDoc */
+	public function getLine( array $feed, RecentChange $rc, $actionComment ) {
+		$lineFromParent = parent::getLine( $feed, $rc, $actionComment );
+		if ( $lineFromParent === null ) {
+			return null;
+		}
+
+		/**
+		 * Don't send renameuser log events to IRC for the
+		 * Wikivy Foundation Trust and Safety team.
+		 */
+		if (
+			$rc->getAttribute( 'rc_source' ) === RecentChange::SRC_LOG &&
+			$rc->getAttribute( 'rc_log_type' ) === 'renameuser'
+		) {
+			$globalUserGroups = CentralAuthUser::getInstanceByName( $rc->getAttribute( 'rc_user_text' ) )->getGlobalGroups();
+			if ( in_array( 'trustandsafety', $globalUserGroups, true ) ) {
+				return null;
+			}
+		}
+
+		$wikiId = WikiMap::getCurrentWikiId();
+
+		// Prefix is \003, no color (\003) switches
+		// back to the term default.
+		return "$wikiId \0035*\003 $lineFromParent";
+	}
+}
