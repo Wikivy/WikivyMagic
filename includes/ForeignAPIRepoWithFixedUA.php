4659fc1a241d5f2773cb5b40bd6aefdafdc425eb
Eric Hocking <ehock7229@outlook.com>
2026-02-24 19:19:36 -0500

    fix error

--- includes/ForeignAPIRepoWithFixedUA.php
+++ includes/ForeignAPIRepoWithFixedUA.php
@@ -8,7 +8,7 @@ use const MW_VERSION;
 
 class ForeignAPIRepoWithFixedUA extends ForeignAPIRepo
 {
-	public static function getUserAgent(): string {
+	public function getUserAgent(): string {
 		$mediaWikiVersion = MW_VERSION;
 		$wikiId = WikiMap::getCurrentWikiId();
 		return "QuickInstantCommons/$mediaWikiVersion MediaWiki/$mediaWikiVersion $wikiId (https://wikivy.com; tech@wikivy.com)";
